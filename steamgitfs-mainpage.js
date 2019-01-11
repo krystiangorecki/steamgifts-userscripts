1c4a53ce0192a47266c5ac59d971260bcdeff192
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2019-01-11 19:46:31 +0100

    Fixed namespace.

--- steamgitfs-mainpage.js
+++ steamgitfs-mainpage.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        steamgitfs-mainpage
-// @namespace   https://github.com/krystiangorecki/steamgifts-mainpage/
+// @namespace   https://github.com/krystiangorecki/steamgifts-userscripts/
 // @author      krystiangorecki
 // @icon        https://cdn.steamgifts.com/img/favicon.ico
 // @match       http://www.steamgifts.com/
@@ -10,7 +10,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant       GM_addStyle
 // @grant       none
-// @version     2019.01.06
+// @version     2019.01.11
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgitfs-mainpage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgitfs-mainpage.js
 // ==/UserScript==
