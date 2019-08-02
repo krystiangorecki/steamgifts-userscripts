323487f5cb1a1fdab3a9f3d7216c1ae360b1cce8
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2019-08-02 12:12:30 +0200

    Updated version.

--- steamgitfs-mainpage.js
+++ steamgitfs-mainpage.js
@@ -10,7 +10,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant       GM_addStyle
 // @grant       none
-// @version     2019.07.24
+// @version     2019.08.02
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgitfs-mainpage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgitfs-mainpage.js
 // ==/UserScript==
