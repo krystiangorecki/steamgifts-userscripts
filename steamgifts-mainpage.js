66a4557b7dc3b56e925ecba8f2e8876360f8f091
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2020-11-09 13:15:16 +0100

    Update steamgifts-mainpage.js

--- steamgifts-mainpage.js
+++ steamgifts-mainpage.js
@@ -10,7 +10,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant       GM_addStyle
 // @grant       none
-// @version     2020.11.07
+// @version     2020.11.09
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // ==/UserScript==
