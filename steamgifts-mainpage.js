04d935e6aeb660e29a4d595e9c0b3f85559a5e23
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2022-07-10 21:50:34 +0200

    Update steamgifts-mainpage.js

--- steamgifts-mainpage.js
+++ steamgifts-mainpage.js
@@ -11,7 +11,7 @@
 // @connect     store.steampowered.com
 // @grant       GM.xmlHttpRequest
 // @grant       GM_addStyle
-// @version     2022.07.09
+// @version     2022.07.10
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // ==/UserScript==
