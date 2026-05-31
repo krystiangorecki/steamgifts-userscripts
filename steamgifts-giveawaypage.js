4f9942452d537dbf281468bfa698e7a65f60cd39
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2026-05-31 21:23:36 +0200

    Update steamgifts-giveawaypage.js

--- steamgifts-giveawaypage.js
+++ steamgifts-giveawaypage.js
@@ -7,12 +7,16 @@
 // @match       https://www.steamgifts.com/giveaway/*
 // @grant       GM.xmlHttpRequest
 // @connect     bazar.lowcygier.pl
-// @version     2023.09.22
+// @version     2026.05.31
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-giveawaypage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-giveawaypage.js
 // ==/UserScript==
 
 var retryCount = 5;
+const delay20min = 20 * 60 * 1000; // 20 min
+const delay30min = 30 * 60 * 1000; // 30 min
+const delay2s = 2 * 1000; // 2s
+const delay5s = 5 * 1000; // 5s
 
 (function () {
     'use strict';
@@ -37,10 +41,12 @@ function autoEnter() {
     var enterLink = document.querySelector(".sidebar__entry-insert");
     if (enterLink == null) {
         var noPointsButton = document.querySelector(".sidebar__error");
-        setTimeout(reloadPage, 1800000); //30min
+        const between20and30minutes = Math.floor(Math.random() * (delay30min - delay30min + 1)) + delay20min;
+        setTimeout(reloadPage, between20and30minutes); // 20-30min
     } else if (enterLink.className.indexOf("is-hidden") == -1) {
         // alert("entry link is not hidden");
-        setTimeout(clickLink, 2000);
+        const between2and5seconds = Math.floor(Math.random() * (delay5s - delay2s + 1)) + delay2s;
+        setTimeout(clickLink, between2and5seconds);
         setTimeout(checkIsDeleteEntryLinkVisible, 7000);
     }
 }
@@ -61,7 +67,8 @@ function checkIsDeleteEntryLinkVisible() {
     } else {
         // alert("delete link is hidden, retrying");
         if (retryCount > 0) {
-            setTimeout(clickLink, 2000);
+            const between2and5seconds = Math.floor(Math.random() * (delay5s - delay2s + 1)) + delay2s;
+            setTimeout(clickLink, between2and5seconds);
             retryCount = retryCount - 1;
         }
     }
@@ -79,7 +86,7 @@ function addLowcyGierBazarLink(){
 }
 
 function getCleanGameTitle(element) {
-  return element.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('+','');
+    return element.innerText.replaceAll('®','').replaceAll('™','').replaceAll(':','').replaceAll('-',' ').replaceAll('—',' ').replaceAll('+','');
 }
 
 function reloadPage() {
