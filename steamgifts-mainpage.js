b61436dd48b7635c80ebb3bd48a1dab0cdee5455
krystiangorecki <10375973+krystiangorecki@users.noreply.github.com>
2026-08-02 18:52:02 +0200

    Update steamgifts-mainpage.js

--- steamgifts-mainpage.js
+++ steamgifts-mainpage.js
@@ -12,13 +12,17 @@
 // @connect     store.steampowered.com
 // @grant       GM.xmlHttpRequest
 // @grant       GM_addStyle
-// @version     2026.08.01
+// @version     2026.08.02
 // @updateURL   https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // @downloadURL https://raw.githubusercontent.com/krystiangorecki/steamgifts-userscripts/master/steamgifts-mainpage.js
 // ==/UserScript==
 
 GM_addStyle(' .separator { border-top:solid red 2px }');
 GM_addStyle(' .ui-tooltip-content {display: none;}');
+GM_addStyle(`
+.pinned-giveaways:not(.pinned-giveaways-expanded) > div:nth-child(n+3) {
+    display: block !important;
+}`);
 
 var BEGINNING = '.page__heading'; //'.pinned-giveaways__outer-wrap';
 
